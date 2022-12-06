22dec3a5cfaa7e4a614eee8a72aae91cd64d1eb5
sohodoll <kovalevvtl@gmail.com>
2022-12-06 16:00:55 +0300

    feat: get the project working

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '513b9a090b4b4072b59c5857cad729b2',
         });
     }
 }
