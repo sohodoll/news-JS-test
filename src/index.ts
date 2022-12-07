81289292c7acfecd3287c12d1ed596e0df1fccc8
sohodoll <kovalevvtl@gmail.com>
2022-12-07 15:24:34 +0300

    feat: finish setup (webpack, TS, ESlint, prettier)

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,8 @@ function init() {
 
 function submitHandler(e: Event) {
     e.preventDefault();
-    const num1 = document.querySelector(
-        "input[name='firstnumber']"
-    ) as HTMLInputElement;
-    const num2 = document.querySelector(
-        "input[name='secondnumber']"
-    ) as HTMLInputElement;
+    const num1 = document.querySelector("input[name='firstnumber']") as HTMLInputElement;
+    const num2 = document.querySelector("input[name='secondnumber']") as HTMLInputElement;
     const result = subtract(Number(num1.value), Number(num2.value));
     const resultElement = document.querySelector('p');
     if (resultElement) {
