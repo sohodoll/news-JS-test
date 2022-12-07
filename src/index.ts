178e1cd9a72eea3bf7ecaad979400649a998de7c
sohodoll <kovalevvtl@gmail.com>
2022-12-07 14:02:14 +0300

    fix: the project can be built

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import App from './components/app/app';
-import './global.css';
+import App from './components/app/app'
+import './global.css'
 
-const app = new App();
-app.start();
+const app = new App()
+app.start()
 
-console.log('comilerd');
+console.log('comiled')
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,49 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path')
+const { merge } = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.ts'),
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.ts$/i,
-                use: ['ts-loader'],
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, './dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-            filename: 'index.html',
-        }),
-        new CleanWebpackPlugin(),
-    ],
-};
+  entry: path.resolve(__dirname, './src/index.ts'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.ts$/i,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html'
+    }),
+    new CleanWebpackPlugin()
+  ]
+}
 
 module.exports = ({ mode }) => {
-    const isProductionMode = mode === 'prod';
-    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+  const isProductionMode = mode === 'prod'
+  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config')
 
-    return merge(baseConfig, envConfig);
-};
+  return merge(baseConfig, envConfig)
+}
