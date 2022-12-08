4056ac3cc3ecd8e508e6e375d6c359ee0e980587
sohodoll <kovalevvtl@gmail.com>
2022-12-08 11:40:49 +0300

    refactor: finish loader.ts

--- src/components/controller/loader.ts
+++ src/components/controller/loader.ts
@@ -1,15 +1,31 @@
 interface ILoader {
     makeUrl(options: object, endpoint: object | string): string;
     errorHandler(res: object): object;
+    load(
+        method: string | undefined,
+        endpoint: object | string,
+        callback: (data: object) => void,
+        options: object
+    ): void;
+    getResp(object: object): void;
 }
 
-interface Response {
-    [index: string]: string | number | object | undefined;
-    statusText: string | undefined;
+interface IGetResp {
+    endpoint: string;
+    options: object;
 }
 
 class Loader implements ILoader {
     constructor(public baseLink: string, public options: object) {}
+
+    getResp(
+        { endpoint, options = {} }: IGetResp,
+        callback = () => {
+            console.error('No callback for GET response');
+        }
+    ) {
+        this.load('GET', endpoint, callback, options);
+    }
 
     makeUrl(options: object, endpoint: object | string): string {
         const urlOptions: { [index: string]: string } = { ...this.options, ...options };
@@ -21,6 +37,7 @@ class Loader implements ILoader {
 
         return url.slice(0, -1);
     }
+
     errorHandler(res: Response): Response {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
@@ -28,6 +45,19 @@ class Loader implements ILoader {
             throw Error(res.statusText);
         }
         return res;
+    }
+
+    load(
+        method: string | undefined,
+        endpoint: object | string,
+        callback: (data: object) => void,
+        options: object = {}
+    ) {
+        fetch(this.makeUrl(options, endpoint), { method })
+            .then(this.errorHandler)
+            .then((res) => res.json())
+            .then((data) => callback(data))
+            .catch((err) => console.error(err));
     }
 }
 
